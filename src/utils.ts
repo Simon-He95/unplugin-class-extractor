945592f2105d790829a8da1c287422206b624cf6
Simon He <674949287@qq.com>
2025-08-04 19:40:48 +0800

    feat: optimize emoji filtering regex in extractorAll function

--- src/utils.ts
+++ src/utils.ts
@@ -180,7 +180,7 @@ export function extractorAll(code: string) {
           return
 
         // 过滤包含emoji表情的内容
-        if (/[\u{1F600}-\u{1F64F}]|[\u{1F300}-\u{1F5FF}]|[\u{1F680}-\u{1F6FF}]|[\u{1F1E0}-\u{1F1FF}]|[\u{2600}-\u{26FF}]|[\u{2700}-\u{27BF}]/u.test(name))
+        if (/[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F1E0}-\u{1F1FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/u.test(name))
           return
 
         // 过滤结尾包含特殊符号的字符串（代码片段）
